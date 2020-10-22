fe04eaec049983a5f4125c3ce1dfbfdd0306b63d
ririyeye <200610237@qq.com>
2020-10-22 10:16:30 +0800

    以太网卡1

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ make ${PRI} zImage -j8
 
 make ${PRI} dtbs
 cat arch/arm/boot/dts/imx6ull-14x14-emmc-7-1024x600-c.dtb >> arch/arm/boot/zImage
-cp arch/arm/boot/dts/imx6ull-14x14-emmc-7-1024x600-c.dtb /tftp/ 
+cp arch/arm/boot/dts/imx6ull-14x14-emmc-7-1024x600-c.dtb /tftp/imx6ull-14x14-emmc-4.3-480x272-c.dtb
 
 cp arch/arm/boot/zImage /tftp
 
