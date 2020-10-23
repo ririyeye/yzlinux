23ea0a136753f1a402773e41a4119225ccb565bb
ririyeye <200610237@qq.com>
2020-10-23 17:28:50 +0800

    添加8200

--- drivers/usb/serial/option.c
+++ drivers/usb/serial/option.c
@@ -343,6 +343,7 @@ static void option_instat_callback(struct urb *urb);
 
 #define ALINK_VENDOR_ID				0x1e0e
 #define SIMCOM_PRODUCT_SIM7100E			0x9001 /* Yes, ALINK_VENDOR_ID */
+#define SIMCOM_VENDOR_ID 0x1e0e
 #define ALINK_PRODUCT_PH300			0x9100
 #define ALINK_PRODUCT_3GU			0x9200
 
@@ -1783,6 +1784,20 @@ static const struct usb_device_id option_ids[] = {
 	{ USB_DEVICE(ALINK_VENDOR_ID, SIMCOM_PRODUCT_SIM7100E),
 	  .driver_info = RSVD(5) | RSVD(6) },
 	{ USB_DEVICE_INTERFACE_CLASS(0x1e0e, 0x9003, 0xff) },	/* Simcom SIM7500/SIM7600 MBIM mode */
+
+
+
+	{ USB_DEVICE(SIMCOM_VENDOR_ID, 0x9001),
+ 	  .driver_info = RSVD(5) | RSVD(6) },
+	{ USB_DEVICE_INTERFACE_CLASS(SIMCOM_VENDOR_ID, 0x9011, 0xff), /* Simcom
+	SIM8200 RNDIS mode ,Reserved the interface for ADB */
+	  .driver_info = RSVD(7) },
+	{ USB_DEVICE(ALINK_VENDOR_ID, SIMCOM_PRODUCT_SIM7100E),
+	  .driver_info = RSVD(5) | RSVD(6) },
+	{ USB_DEVICE_INTERFACE_CLASS(0x1e0e, 0x9011, 0xff),
+	  .driver_info = RSVD(7) },
+
+
 	{ USB_DEVICE(ALCATEL_VENDOR_ID, ALCATEL_PRODUCT_X060S_X200),
 	  .driver_info = NCTRL(0) | NCTRL(1) | RSVD(4) },
 	{ USB_DEVICE(ALCATEL_VENDOR_ID, ALCATEL_PRODUCT_X220_X500D),
