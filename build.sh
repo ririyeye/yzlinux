0048e618a9602cfbccaeacf56fdbb54442bb8f6d
ririyeye <200610237@qq.com>
2020-11-09 22:11:55 +0800

    111

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ ARCH=arm
 PRI="CROSS_COMPILE=arm-linux-gnueabihf- ARCH=arm"
 #make ${PRI} imx_alientek_emmc_defconfig
 make ${PRI} menuconfig
-make ${PRI} zImage -j8
+make ${PRI} zImage -j16
 
 make ${PRI} dtbs
 cat arch/arm/boot/dts/imx6ull-14x14-emmc-7-1024x600-c.dtb >> arch/arm/boot/zImage
